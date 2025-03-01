79e7936d2a40343894cea44b7927687f8d4907b4
KarolNiewiadomski <karolniewiadomski@outlook.com>
2025-03-01 13:15:59 +0100

    added paths to login and register pages

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-import Home from "./components/home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+import Home from "./components/Home";
+import Login from "./components/pages/Login";
+import Register from "./components/pages/Register";
 
 const App = () => {
   return (
