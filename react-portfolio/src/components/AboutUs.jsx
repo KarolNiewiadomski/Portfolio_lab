cfc71098f36227ecf4e14b4a44589e06d8d42c27
KarolNiewiadomski <karolniewiadomski@outlook.com>
2025-03-02 19:10:17 +0100

    completed about us component

--- react-portfolio/src/components/AboutUs.jsx
+++ react-portfolio/src/components/AboutUs.jsx
@@ -3,6 +3,15 @@ import React from "react";
 const AboutUs = () => {
   return (
     <div id="AboutUs">
+      <div>
+        <h2>About us</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Reprehenderit architecto eaque porro eveniet tenetur nemo nisi.
+          Blanditiis quis consequatur quos qui ut. Eveniet, porro. Incidunt
+          repellat porro inventore commodi animi.
+        </p>
+      </div>
       <img src="#" alt="Group of people" />
     </div>
   );
