21c8bbc5964ce87f34b42ff8c4eca7cbe50d0134
KarolNiewiadomski <karolniewiadomski@outlook.com>
2025-03-08 13:17:37 +0100

    corrected the alignment of the emelents to look like the plan of the project

--- react-portfolio/src/components/HomeMain.jsx
+++ react-portfolio/src/components/HomeMain.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Decoration from "../Assets/Decoration.svg";
 
 const HomeMain = () => {
   return (
-    <section id="HomeMain">
+    <section id="HomeMain" className="home-main-container">
       <h1>Start helping!</h1>
       <h2>Give back unwanted stuff in trusted hands</h2>
+      <img src={Decoration} alt="Decoration" />
       <Link to="/Login">
         <button>Give back</button>
       </Link>
