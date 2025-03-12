fb38f226c16aa6d66571e297b50b2cd8befd303e
KarolNiewiadomski <karolniewiadomski@outlook.com>
2025-03-12 17:26:28 +0100

    corrected styling to all components and added images

--- react-portfolio/src/components/Steps.jsx
+++ react-portfolio/src/components/Steps.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import Decoration from "../Assets/Decoration.svg";
+import Tshirt from "../Assets/Icon-1.svg";
+import ShoppingBag from "../Assets/Icon-2.svg";
+import MagnifingGlass from "../Assets/Icon-3.svg";
+import Recycling from "../Assets/Icon-4.svg";
 
 const Steps = () => {
   return (
@@ -13,31 +17,33 @@ const Steps = () => {
 
       <div className="steps-instruction">
         <div>
-          <img src="" alt="T-shirt" />
+          <img src={Tshirt} alt="T-shirt" />
           <p>Pick stuff</p>
           <p>Clothing, toys, equipment & other</p>
         </div>
 
         <div>
-          <img src="" alt="Shopping bag" />
+          <img src={ShoppingBag} alt="Shopping bag" />
           <p>Pack them</p>
           <p>Use garbage bag</p>
         </div>
 
         <div>
-          <img src="" alt="Magnifing glass" />
+          <img src={MagnifingGlass} alt="Magnifing glass" />
           <p>Decide who to help</p>
           <p>Chose a trusted location</p>
         </div>
 
         <div>
-          <img src="" alt="Recycle icon" />
+          <img src={Recycling} alt="Recycle icon" />
           <p>Order pickup</p>
           <p>Pickup will arrive on schedule</p>
         </div>
       </div>
 
-      <Link to="/Login">Donate gifts</Link>
+      <Link to="/Login" className="action-button">
+        Donate gifts
+      </Link>
     </section>
   );
 };
