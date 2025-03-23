2a2b3aa05ba2e1909da9c4de644aeb0a6e29e265
KarolNiewiadomski <karolniewiadomski@outlook.com>
2025-03-23 15:02:42 +0100

    added filtering to who we help component

--- react-portfolio/src/components/WhoWeHelp.jsx
+++ react-portfolio/src/components/WhoWeHelp.jsx
@@ -3,6 +3,7 @@ import Decoration from "../Assets/Decoration.svg";
 
 const WhoWeHelp = () => {
   const [activeCategory, setActiveCategory] = useState("Charities");
+
   return (
     <section id="FO" className="who-we-help">
       <div>
@@ -28,227 +29,233 @@ const WhoWeHelp = () => {
           soluta a dicta porro nemo ipsam repudiandae veritatis.
         </p>
 
-        <ul className="charity-list">
-          <li className="charities">
-            <div>
-              <h2>Charity "Dbam o zdrowie"</h2>
-              <p>
-                Goal and mission: Help people who are in difficult live
-                situations.
-              </p>
-            </div>
-            <div>
-              <p>clothes, food, home appliences, toys</p>
-            </div>
-          </li>
+        {activeCategory === "Charities" && (
+          <ul className="charity-list">
+            <li className="charities">
+              <div>
+                <h2>Charity "Dbam o zdrowie"</h2>
+                <p>
+                  Goal and mission: Help people who are in difficult live
+                  situations.
+                </p>
+              </div>
+              <div>
+                <p>clothes, food, home appliences, toys</p>
+              </div>
+            </li>
 
-          <li className="charities">
-            <div>
-              <h2>Charity "Dla dzieci"</h2>
-              <p>Goal and mission: Help kids from poor families.</p>
-            </div>
-            <div>
-              <p>clothes, home appliences, toys</p>
-            </div>
-          </li>
+            <li className="charities">
+              <div>
+                <h2>Charity "Dla dzieci"</h2>
+                <p>Goal and mission: Help kids from poor families.</p>
+              </div>
+              <div>
+                <p>clothes, home appliences, toys</p>
+              </div>
+            </li>
 
-          <li className="charities">
-            <div>
-              <h2>Charity "Bez domu"</h2>
-              <p>Goal and mission: Help people without a home.</p>
-            </div>
-            <div>
-              <p>clothes, food, blankets</p>
-            </div>
-          </li>
+            <li className="charities">
+              <div>
+                <h2>Charity "Bez domu"</h2>
+                <p>Goal and mission: Help people without a home.</p>
+              </div>
+              <div>
+                <p>clothes, food, blankets</p>
+              </div>
+            </li>
 
-          <li className="charities">
-            <div>
-              <h2>Charity "Dbam o zdrowie"</h2>
-              <p>
-                Goal and mission: Help people who are in difficult live
-                situations.
-              </p>
-            </div>
-            <div>
-              <p>clothes, food, home appliences, toys</p>
-            </div>
-          </li>
+            <li className="charities">
+              <div>
+                <h2>Charity "Dbam o zdrowie"</h2>
+                <p>
+                  Goal and mission: Help people who are in difficult live
+                  situations.
+                </p>
+              </div>
+              <div>
+                <p>clothes, food, home appliences, toys</p>
+              </div>
+            </li>
 
-          <li className="charities">
-            <div>
-              <h2>Charity "Dla dzieci"</h2>
-              <p>Goal and mission: Help kids from poor families.</p>
-            </div>
-            <div>
-              <p>clothes, home appliences, toys</p>
-            </div>
-          </li>
+            <li className="charities">
+              <div>
+                <h2>Charity "Dla dzieci"</h2>
+                <p>Goal and mission: Help kids from poor families.</p>
+              </div>
+              <div>
+                <p>clothes, home appliences, toys</p>
+              </div>
+            </li>
 
-          <li className="charities">
-            <div>
-              <h2>Charity "Bez domu"</h2>
-              <p>Goal and mission: Help people without a home.</p>
-            </div>
-            <div>
-              <p>clothes, food, blankets</p>
-            </div>
-          </li>
+            <li className="charities">
+              <div>
+                <h2>Charity "Bez domu"</h2>
+                <p>Goal and mission: Help people without a home.</p>
+              </div>
+              <div>
+                <p>clothes, food, blankets</p>
+              </div>
+            </li>
 
-          <li className="charities">
-            <div>
-              <h2>Charity "Dbam o zdrowie"</h2>
-              <p>
-                Goal and mission: Help people who are in difficult live
-                situations.
-              </p>
-            </div>
-            <div>
-              <p>clothes, food, home appliences, toys</p>
-            </div>
-          </li>
+            <li className="charities">
+              <div>
+                <h2>Charity "Dbam o zdrowie"</h2>
+                <p>
+                  Goal and mission: Help people who are in difficult live
+                  situations.
+                </p>
+              </div>
+              <div>
+                <p>clothes, food, home appliences, toys</p>
+              </div>
+            </li>
 
-          <li className="charities">
-            <div>
-              <h2>Charity "Dla dzieci"</h2>
-              <p>Goal and mission: Help kids from poor families.</p>
-            </div>
-            <div>
-              <p>clothes, home appliences, toys</p>
-            </div>
-          </li>
+            <li className="charities">
+              <div>
+                <h2>Charity "Dla dzieci"</h2>
+                <p>Goal and mission: Help kids from poor families.</p>
+              </div>
+              <div>
+                <p>clothes, home appliences, toys</p>
+              </div>
+            </li>
 
-          <li className="charities">
-            <div>
-              <h2>Charity "Bez domu"</h2>
-              <p>Goal and mission: Help people without a home.</p>
-            </div>
-            <div>
-              <p>clothes, food, blankets</p>
-            </div>
-          </li>
-        </ul>
+            <li className="charities">
+              <div>
+                <h2>Charity "Bez domu"</h2>
+                <p>Goal and mission: Help people without a home.</p>
+              </div>
+              <div>
+                <p>clothes, food, blankets</p>
+              </div>
+            </li>
+          </ul>
+        )}
 
-        <ul className="non-profit-list">
-          <li className="non-profit">
-            <div>
-              <h2>Non-profit</h2>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Temporibus, unde consequatur.
-              </p>
-            </div>
-            <div>
-              <p>Lorem ipsum dolor sit amet consectetur.</p>
-            </div>
-          </li>
+        {activeCategory === "Non-profit" && (
+          <ul className="non-profit-list">
+            <li className="non-profit">
+              <div>
+                <h2>Non-profit</h2>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Temporibus, unde consequatur.
+                </p>
+              </div>
+              <div>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+              </div>
+            </li>
 
-          <li className="non-profit">
-            <div>
-              <h2>Non-profit</h2>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Temporibus, unde consequatur.
-              </p>
-            </div>
-            <div>
-              <p>Lorem ipsum dolor sit amet consectetur.</p>
-            </div>
-          </li>
+            <li className="non-profit">
+              <div>
+                <h2>Non-profit</h2>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Temporibus, unde consequatur.
+                </p>
+              </div>
+              <div>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+              </div>
+            </li>
 
-          <li className="non-profit">
-            <div>
-              <h2>Non-profit</h2>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Temporibus, unde consequatur.
-              </p>
-            </div>
-            <div>
-              <p>Lorem ipsum dolor sit amet consectetur.</p>
-            </div>
-          </li>
+            <li className="non-profit">
+              <div>
+                <h2>Non-profit</h2>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Temporibus, unde consequatur.
+                </p>
+              </div>
+              <div>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+              </div>
+            </li>
 
-          <li className="non-profit">
-            <div>
-              <h2>Non-profit</h2>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Temporibus, unde consequatur.
-              </p>
-            </div>
-            <div>
-              <p>Lorem ipsum dolor sit amet consectetur.</p>
-            </div>
-          </li>
+            <li className="non-profit">
+              <div>
+                <h2>Non-profit</h2>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Temporibus, unde consequatur.
+                </p>
+              </div>
+              <div>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+              </div>
+            </li>
 
-          <li className="non-profit">
-            <div>
-              <h2>Non-profit</h2>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Temporibus, unde consequatur.
-              </p>
-            </div>
-            <div>
-              <p>Lorem ipsum dolor sit amet consectetur.</p>
-            </div>
-          </li>
+            <li className="non-profit">
+              <div>
+                <h2>Non-profit</h2>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Temporibus, unde consequatur.
+                </p>
+              </div>
+              <div>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+              </div>
+            </li>
 
-          <li className="non-profit">
-            <div>
-              <h2>Non-profit</h2>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Temporibus, unde consequatur.
-              </p>
-            </div>
-            <div>
-              <p>Lorem ipsum dolor sit amet consectetur.</p>
-            </div>
-          </li>
-        </ul>
+            <li className="non-profit">
+              <div>
+                <h2>Non-profit</h2>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Temporibus, unde consequatur.
+                </p>
+              </div>
+              <div>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+              </div>
+            </li>
+          </ul>
+        )}
 
-        <ul className="local-lsit">
-          <li className="local">
-            <div>
-              <h2>Local</h2>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Temporibus, unde consequatur.
-              </p>
-            </div>
-            <div>
-              <p>Lorem ipsum dolor sit amet consectetur.</p>
-            </div>
-          </li>
+        {activeCategory === "Local" && (
+          <ul className="local-lsit">
+            <li className="local">
+              <div>
+                <h2>Local</h2>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Temporibus, unde consequatur.
+                </p>
+              </div>
+              <div>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+              </div>
+            </li>
 
-          <li className="local">
-            <div>
-              <h2>Local</h2>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Temporibus, unde consequatur.
-              </p>
-            </div>
-            <div>
-              <p>Lorem ipsum dolor sit amet consectetur.</p>
-            </div>
-          </li>
+            <li className="local">
+              <div>
+                <h2>Local</h2>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Temporibus, unde consequatur.
+                </p>
+              </div>
+              <div>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+              </div>
+            </li>
 
-          <li className="local">
-            <div>
-              <h2>Local</h2>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Temporibus, unde consequatur.
-              </p>
-            </div>
-            <div>
-              <p>Lorem ipsum dolor sit amet consectetur.</p>
-            </div>
-          </li>
-        </ul>
+            <li className="local">
+              <div>
+                <h2>Local</h2>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Temporibus, unde consequatur.
+                </p>
+              </div>
+              <div>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+              </div>
+            </li>
+          </ul>
+        )}
       </div>
     </section>
   );
